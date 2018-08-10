0c2b7bb0b309e88b8ce927e5f5a780e119ce487a
Paulifer <pauli.hua.timm@gmail.com>
2018-08-10 12:02:39 -0400

    me funciona traer saldo y lo imprime

--- src/js/saldo.js
+++ src/js/saldo.js
@@ -1,13 +1,27 @@
 
-function bip(){
-	fetch('http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip={id}')
+function saldobip(){
+	let numTarjeta = boxsaldo.value;
+	let selectarj = selectarjeta.value;
+	fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${numTarjeta || selectarj}`)
 	.then(res =>{
 		return res.json();
 	})
 	.then(data =>{
 		console.log(data);
+		tarjetas(data);
 	})
 	.catch(error =>{
 		console.log(error);
 	})
+	numTarjeta = '';
+}
+const tarjetas = data =>{
+	for(let i in data){
+		const regexp = /(\d+)/g;
+		const saldosBip = data.saldoTarjeta.match(regexp);
+		const saldoOk = parseInt(saldosBip[0]+saldosBip[1]);
+		contenedorSaldo.innerHTML = '$' + saldoOk ;	 
+		console.log(saldosBip);
+		console.log(saldoOk);
+	};
 }
